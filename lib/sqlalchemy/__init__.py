7d8fa11e9a171593803334a8bb02527091d1831f
Kent Bower <kb@retailarchitects.com>
2021-12-23 07:18:24 -0500

    release 0.7.623kb PostgreSQL CREATE INDEX on other schema bug https://github.com/sqlalchemy/sqlalchemy/commit/44576792ab80540abbf7da88badd5bd05953c478

--- lib/sqlalchemy/__init__.py
+++ lib/sqlalchemy/__init__.py
@@ -117,7 +117,7 @@ from sqlalchemy.engine import create_engine, engine_from_config
 __all__ = sorted(name for name, obj in locals().items()
                  if not (name.startswith('_') or inspect.ismodule(obj)))
 
-__version__ = '0.7.622kb'
+__version__ = '0.7.623kb'
 
 del inspect, sys
 
